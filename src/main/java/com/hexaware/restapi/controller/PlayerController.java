194541415bfae81c6890248e61ced732f20233cc
Sai_vighnessh <saivighnesshbalaji@gmail.com>
2025-06-11 11:36:17 +0530

    backend

--- src/main/java/com/hexaware/restapi/controller/PlayerController.java
+++ src/main/java/com/hexaware/restapi/controller/PlayerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.hexaware.restapi.service.PlayerServiceImpl;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/players")
 public class PlayerController {
